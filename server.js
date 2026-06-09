e0bf7e6d89ef4b7c661dc9ed5f9606965f31921f
Karany24 <ky334219@gmail.com>
2026-06-09 19:08:46 +0530

    Fixed port for deployment

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ io.on('connection', (socket) => {
 })
 
 
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 server.listen(PORT, () => {
     console.log(`Ther serve is running on port ${PORT}`);
 
